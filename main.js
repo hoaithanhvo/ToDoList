a15b9c130b51c9f8aecbdf0f7e68869025a3c8ba
hoaithanhvo <vohoaithanh52@gmail.com>
2023-04-16 00:01:08 +0700

    update code

--- main.js
+++ main.js
@@ -4,25 +4,30 @@ const list = document.getElementById("task-list")
 const deleteAll = document.getElementById("deletAll")
 const last = document.getElementById("last")
 
-const tasks = []
+let tasks = []
 var tasks1 = []
 // localStorage.setItem('tasks', JSON.stringify(tasks));
-let string = localStorage.getItem("tasks")
-var myArray = JSON.parse(string)
-last.innerHTML = "";
-tasks1 = [...myArray]
-console.log("d", tasks1);
-tasks1.forEach((item, index) => {
+function start() {
+    let string = localStorage.getItem("tasks")
+    var myArray = JSON.parse(string)
+    last.innerHTML = "";
+    tasks1 = [...myArray]
+    console.log("d", tasks1);
+    tasks1.forEach((item, index) => {
 
-    const taomoi1 = document.createElement("h1")
-    taomoi1.innerHTML = `<h1>${item}</h1>
+        const taomoi1 = document.createElement("h1")
+        taomoi1.innerHTML = `<h1>${item}</h1>
             <button class="delete-button" data-index=${index}>xóa</button>
             <button class="edit-button" data-index=${index}>edit</button>
         `
-    last.appendChild(taomoi1)
-})
+        last.appendChild(taomoi1)
+    })
+}
 
 function render() {
+    if (localStorage.getItem('tasks')) {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
     list.innerHTML = "";
 
 
@@ -51,21 +56,13 @@ function render() {
 }
 // console.log("myArray12", tasks1);
 
-
-
-
-
-
-
-
-
 function addTask() {
     const task = input.value
     if (task) {
         tasks.push(task)
         input.value = ""
-        render()
         localStorage.setItem('tasks', JSON.stringify(tasks));
+        render()
 
     }
 }
@@ -73,20 +70,28 @@ function edit(index) {
     const newTask = prompt("Enter new task:");
     if (newTask) {
         tasks[index] = newTask.trim();
+        localStorage.setItem('tasks', JSON.stringify(tasks));
         render();
     }
 }
 function deleteTask(index) {
     tasks[index] = null;
+    tasks.splice(index, 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
     render();
+    // start()
 }
 
 function XoaTatCa() {
     tasks.splice(0, tasks.length);
+    console.log(tasks);
     deleteAll.style.color = "red"
+    localStorage.removeItem('tasks');
+
 
 
     render()
+    // start()
 }
 
 deleteAll.addEventListener("click", XoaTatCa)
@@ -108,5 +113,5 @@ list.addEventListener("click", event => {
     }
 })
 
-// console.log("local", localStorage.getItem(0));
 render();
+// start();
